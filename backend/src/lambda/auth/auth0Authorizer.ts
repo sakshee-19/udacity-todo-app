05355360df187ee0a8dc051bd9c14021ca5fe67e
saksjain <saksjain@adobe.com>
2020-05-28 00:08:05 +0530

    add rsa authentication for create todo

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,15 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-//verify,
-import {  decode } from 'jsonwebtoken'
+
+import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
+import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://sakshee-19.auth0.com/.well-known/jwks.json'
 console.log("processing ", jwksUrl)
 
 export const handler = async (
@@ -59,10 +56,22 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
   console.log("jwt ",jwt)
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if (!jwt) {
+    throw new Error('Invalid token')
+  }
+  try{
+  const signed = await Axios.get(jwksUrl)
+  // const keys = signed.data.keys
+
+  // if (!keys || !keys.length) {
+  //   throw new Error('The JWKS endpoint did not contain any keys');
+  // }
+  // const signingKey = keys.find(key => key.kid === kid);
+
+  return verify(token, signed.data, { algorithms: ['RS256'] }) as JwtPayload;
+  } catch(e) {
+    logger.info("error captured ", {error: e})
+  }
 }
 
 function getToken(authHeader: string): string {
